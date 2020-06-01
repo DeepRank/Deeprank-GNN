87432d46197f7aaae6ec99ade9971d9862d0d2cc
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-06-01 16:54:39 +0200

    fix example

--- example/create_graph.py
+++ example/create_graph.py
@@ -1,8 +1,8 @@
 from graphprot.GraphGenMP import GraphHDF5
 
-pdb_path = './pdb/1ATN/'
-pssm_path = './pssm/1ATN/'
-ref = './ref/1ATN/'
+pdb_path = './data/pdb/1ATN/'
+pssm_path = './data/pssm/1ATN/'
+ref = './data/ref/1ATN/'
 
 GraphHDF5(pdb_path=pdb_path, ref_path=ref, pssm_path=pssm_path,
           graph_type='residue', outfile='1ATN_residue.hdf5',
--- example/nn.py
+++ example/nn.py
@@ -4,13 +4,14 @@ from graphprot.foutnet import FoutNet
 
 database = './hdf5/1ACB_residue.hdf5'
 
-NN = NeuralNet(database, GINet
+NN = NeuralNet(database, GINet,
                node_feature=['type', 'polarity', 'bsa',
                              'depth', 'hse', 'ic', 'pssm'],
                edge_feature=['dist'],
                target='irmsd',
                index=range(400),
-               batch_size=64, percent=[0.8, 0.2])
+               batch_size=64,
+               percent=[0.8, 0.2])
 
 NN.train(nepoch=250, validate=False)
 NN.plot_scatter()
