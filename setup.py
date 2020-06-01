985b6836f7c0032ef7ab9d33117cb3dfec1d047e
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-06-01 12:45:19 +0200

    improve setup and repo

--- setup.py
+++ setup.py
@@ -1,22 +1,53 @@
-#from distutils.core import setup
-from setuptools import setup
+#!/usr/bin/env python
+
+import os
+
+from setuptools import (find_packages, setup)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# To update the package version number, edit GraphProt/__version__.py
+version = {}
+with open(os.path.join(here, 'graphprot', '__version__.py')) as f:
+    exec(f.read(), version)
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 
 setup(
     name='GraphProt',
+    version=version['__version__'],
     description='Graph Neural network Scoring of protein-protein conformations',
-    version='0.0',
+    long_description=readme + '\n\n',
+    long_description_content_type='text/markdown',
+    author=["Nicolas Renaud"],
+    author_email='n.renaud@esciencecenter.nl',
     url='https://github.com/DeepRank/GraphProt',
-    packages=['graphprot'],
+    packages=find_packages(),
+    package_dir={'graphprot': 'graphprot'},
+    include_package_data=True,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='graphprot',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Chemistry'
+    ],
+    test_suite='tests',
 
 
-    install_requires=[
-        'numpy >= 1.13',
-        'scipy'],
-        #'tarfiles',
-        #'pickle'],
+    install_requires=['numpy >= 1.13', 'scipy', 'h5py', 'torch',
+                      'torch-geometric', 'networkx'],
 
-    extras_require= {
-        'test': ['nose', 'coverage', 'pytest',
-                 'pytest-cov','codacy-coverage','coveralls'],
+    extras_require={
+        'dev': ['prospector[with_pyroma]', 'yapf', 'isort'],
+        'doc': ['recommonmark', 'sphinx', 'sphinx_rtd_theme'],
+        'test': ['coverage', 'pycodestyle', 'pytest', 'pytest-cov',
+                 'pytest-runner'],
     }
 )
