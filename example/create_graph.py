8519b8d8577af886cc36ac5c0b2a4173a2f9a212
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-11-11 17:05:20 +0100

    changed pdb to have same residues

--- example/create_graph.py
+++ example/create_graph.py
@@ -6,4 +6,4 @@ ref = './data/ref/1ATN/'
 
 GraphHDF5(pdb_path=pdb_path, ref_path=ref, pssm_path=pssm_path,
           graph_type='residue', outfile='1ATN_residue.hdf5',
-          nproc=2, tmpdir='./tmpdir')
+          nproc=1, tmpdir='./tmpdir')
