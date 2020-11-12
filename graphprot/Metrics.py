d0c8f486fe0188d90a08fce970794f10412b1a05
manonreau <manon.reau@gmail.com>
2020-11-12 21:56:10 +0100

    Update Metrics.py

--- graphprot/Metrics.py
+++ graphprot/Metrics.py
@@ -8,7 +8,7 @@ from sklearn.metrics import confusion_matrix
 def get_boolean(vals, threshold, target):
         
         if target == 'fnat' or target == 'bin':
-                vals_bool = [1 if x >= threshold else 0 for x in vals]
+                vals_bool = [1 if x > threshold else 0 for x in vals]
         else: 
                 vals_bool = [1 if x < threshold else 0 for x in vals]
         
