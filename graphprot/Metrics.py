3bbeeca8c7f0f11ddc06e2b842fde47548c38e21
manonreau <manon.reau@gmail.com>
2020-12-15 10:33:51 +0100

    Update Metrics.py

--- graphprot/Metrics.py
+++ graphprot/Metrics.py
@@ -8,7 +8,8 @@ from sklearn.metrics import confusion_matrix
 
 def get_boolean(values, threshold, target):
 
-    if target == 'fnat' or target == 'bin':
+    inverse = ['fnat', 'bin']
+    if target in inverse:
         values_bool = [1 if x > threshold else 0 for x in values]
     else:
         values_bool = [1 if x < threshold else 0 for x in values]
@@ -120,7 +121,8 @@ class Metrics(object):
 
         idx = np.argsort(self.prediction)
 
-        if self.target == 'fnat' or self.target == 'bin':
+        inverse = ['fnat', 'bin']
+        if self.target in inverse:   
             idx = idx[::-1]
 
         ground_truth_bool = get_boolean(
