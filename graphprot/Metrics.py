afad20c46ebf7b1e5bc62376a6d10acc2392a734
manonreau <manon.reau@gmail.com>
2021-01-07 14:14:22 +0100

    Update Metrics.py

--- graphprot/Metrics.py
+++ graphprot/Metrics.py
@@ -118,75 +118,45 @@ class Metrics(object):
         self.accuracy = (true_positive+true_negative)/(true_positive+false_positive+false_negative+true_negative)
 
         # regression metrics
-        if target == 'fnat' or target == 'irmsd' or target == 'lrmsd':
+        self.explained_variance = None
+        self.max_error =  None
+        self.mean_abolute_error = None
+        self.mean_squared_error = None
+        self.root_mean_squared_error = None
+        self.mean_squared_log_error = None
+        self.median_squared_log_error = None
+        self.r2_score = None
+        
+        if target in ['fnat', 'irmsd', 'lrmsd']:
+
+            # Explained variance regression score function
+            self.explained_variance = metrics.explained_variance_score(self.y, self.prediction)
             
-            try:
-                # Explained variance regression score function
-                self.explained_variance = metrics.explained_variance_score(self.y, self.prediction)
-            except: 
-                self.explained_variance = None
-                
-            try :
-                # Max_error metric calculates the maximum residual error
-                self.max_error = metrics.max_error(self.y, self.prediction)
-            except:
-                self.max_error = None
-              
-            try:
-                # Mean absolute error regression loss
-                self.mean_absolute_error = metrics.mean_absolute_error(self.y, self.prediction)
-            except:
-                self.mean_absolute_error = None
-                
-            try:    
-                # Mean squared error regression loss
-                self.mean_squared_error = metrics.mean_squared_error(self.y, self.prediction, squared = True)
-            except:
-                self.mean_squared_error = None
+            # Max_error metric calculates the maximum residual error
+            self.max_error = metrics.max_error(self.y, self.prediction)
+
+            # Mean absolute error regression loss
+            self.mean_absolute_error = metrics.mean_absolute_error(self.y, self.prediction)
             
-            try:
-                # Root mean squared error regression loss
-                self.root_mean_squared_error = metrics.mean_squared_error(self.y, self.prediction, squared = False)
-            except:
-                self.root_mean_squared_error = None
+            # Mean squared error regression loss
+            self.mean_squared_error = metrics.mean_squared_error(self.y, self.prediction, squared = True)
+
+            # Root mean squared error regression loss
+            self.root_mean_squared_error = metrics.mean_squared_error(self.y, self.prediction, squared = False)
             
-            try:
-                # Mean squared logarithmic error regression loss
-                self.mean_squared_log_error = metrics.mean_squared_log_error(self.y, self.prediction)
-            except:
-                self.mean_squared_log_error = None
-             
-            try:
-                # Median absolute error regression loss
-                self.median_squared_log_error = metrics.median_absolute_error(self.y, self.prediction)
-            except: 
-                self.median_squared_log_error = None
+            # Mean squared logarithmic error regression loss
+            self.mean_squared_log_error = metrics.mean_squared_log_error(self.y, self.prediction)
             
-            try: 
-                # R^2 (coefficient of determination) regression score function
-                self.r2_score = metrics.r2_score(self.y, self.prediction)
-            except:
-                self.r2_score = None
-                
-        else: 
-            self.explained_variance = None
-            self.max_error =  None
-            self.mean_abolute_error = None
-            self.mean_squared_error = None
-            self.root_mean_squared_error = None
-            self.mean_squared_log_error = None
-            self.median_squared_log_error = None
-            self.r2_score = None
+            # Median absolute error regression loss
+            self.median_squared_log_error = metrics.median_absolute_error(self.y, self.prediction)
+
+            # R^2 (coefficient of determination) regression score function
+            self.r2_score = metrics.r2_score(self.y, self.prediction)
+            
             
     def format_score(self):
         
         '''
-        input : 
-        - dtype: hitrate -> considers 0 values as the best 
-                            need to invert fnat and binary classification
-                 auc     -> consider larger 1 values as the best 
-                            need to invert 
-
         output : 
         - idx: value rank 
         - ground_truth_bool: binary y values
@@ -206,13 +176,11 @@ class Metrics(object):
     def hitrate(self):
 
         idx, ground_truth_bool = self.format_score()
-        hitrate = np.cumsum(ground_truth_bool[idx])
-
-        return hitrate
+        
+        return np.cumsum(ground_truth_bool[idx])
 
     def auc(self):
         
         idx, ground_truth_bool = self.format_score()
-        auc_value = roc_auc_score(ground_truth_bool, idx)
+        return roc_auc_score(ground_truth_bool, idx)
 
-        return auc_value
