529d233620431da339a3d6e60e3c3f576d05526c
manonreau <manon.reau@gmail.com>
2021-05-12 16:24:23 +0200

    Update EGAT.py

--- deeprank_gnn/EGAT.py
+++ deeprank_gnn/EGAT.py
@@ -7,20 +7,16 @@ import torch.nn as nn
 class EGAT(torch.nn.Module):
     """
     1) Create edges feature by concatenating node feature
-    .. math::
-    $e_{ij} = LeakyReLu (a_{ij} * [W * x_i || W * x_j])$
+    :math: $e_{ij} = LeakyReLu (a_{ij} * [W * x_i || W * x_j])$
     
     2) Apply softmax function, in order to learn to ignore some edges
-    .. math::
-    $\alpha_{ij}  = softmax(e_{ij})$
+    :math: $\alpha_{ij}  = softmax(e_{ij})$
     
     3) Sum over the nodes (no averaging here !)
-    .. math::
-    $z_i = \sum_j (\alpha_{ij} * Wx_j + b_i)$
+    :math: $z_i = \sum_j (\alpha_{ij} * Wx_j + b_i)$
     
     Herein, we add the edge feature to the step 1)
-    .. math::
-    $e_{ij} = LeakyReLu (a_{ij} * [W * x_i || W * x_j || We * edge_{attr} ])$
+    :math: $e_{ij} = LeakyReLu (a_{ij} * [W * x_i || W * x_j || We * edge_{attr} ])$
     """
 
     def __init__(self,
