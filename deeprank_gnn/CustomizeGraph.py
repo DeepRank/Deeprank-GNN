66fb7d0059bb6f64bcc84e2d18d268aa8a1f508c
Manon F.  Réau <mfreau@int2.bullx>
2021-05-20 18:27:42 +0200

    add_target

--- deeprank_gnn/CustomizeGraph.py
+++ deeprank_gnn/CustomizeGraph.py
@@ -9,18 +9,20 @@ def add_target(graph_path, target_name, target_list, sep=' '):
 
     # Converts the target list into a dictionary
     # The input target list should respect the following format :
-    # 1ATN_xxx-1.pdb 0
-    # 1ATN_xxx-2.pdb 1
-    # 1ATN_xxx-3.pdb 0
-    # 1ATN_xxx-4.pdb 0
-    labels, values = np.genfromtxt(target_list, delimiter=sep, unpack=True)
+    # 1ATN_xxx-1 0
+    # 1ATN_xxx-2 1
+    # 1ATN_xxx-3 0
+    # 1ATN_xxx-4 0
+
+    labels = np.loadtxt(target_list, delimiter=sep, usecols=[0], dtype=np.str)
+    values = np.loadtxt(target_list, delimiter=sep, usecols=[1])
     for label, value in zip(labels, values):
         target_dict[label] = value
-        
+
     first_model = True
 
     for hdf5 in glob.glob('{}/*.hdf5'.format(graph_path)):
-        try:
+        if True:
             f5 = h5py.File(hdf5, 'a')
             for model in f5.keys():
                 group=f5['{}/score/'.format(model)]
@@ -36,9 +38,9 @@ def add_target(graph_path, target_name, target_list, sep=' '):
 
                 # Create the target     
                 group[target_name] = target_dict[model]
-
-                f5.close()
-
-        except:
-            print('no graph for {}'.format(hdf5))
+                
+            f5.close()
+            
+        #except:
+        #    print('no graph for {}'.format(hdf5))
 
