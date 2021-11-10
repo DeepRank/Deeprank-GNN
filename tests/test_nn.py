5ae04dad6b405a8629ef8d814e07c4b1c1d7fbcd
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-11-10 17:47:39 +0100

    added test for foutnet

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -13,7 +13,7 @@ class TestNeuralNet(unittest.TestCase):
     def setUp(self):
         self.database = 'tests/hdf5/1ATN_residue.hdf5'
 
-    def test_neural_net(self):
+    def test_ginet(self):
         NN = NeuralNet(self.database, GINet,
                        node_feature=['type', 'polarity', 'bsa',
                                      'depth', 'hse', 'ic', 'pssm'],
@@ -29,6 +29,27 @@ class TestNeuralNet(unittest.TestCase):
         NN.save_model('test.pth.tar')
 
         NN_cpy = NeuralNet(self.database, GINet,
+                           pretrained_model='test.pth.tar')
+
+        if __PLOT__:
+            NN.plot_scatter()
+
+    def test_fout(self):
+        NN = NeuralNet(self.database, FoutNet,
+                       node_feature=['type', 'polarity', 'bsa',
+                                     'depth', 'hse', 'ic', 'pssm'],
+                       edge_feature=['dist'],
+                       target='irmsd',
+                       index=None,
+                       task='reg',
+                       batch_size=64,
+                       percent=[0.8, 0.2])
+
+        NN.train(nepoch=5, validate=False)
+
+        NN.save_model('test.pth.tar')
+
+        NN_cpy = NeuralNet(self.database, FoutNet,
                            pretrained_model='test.pth.tar')
 
         if __PLOT__:
