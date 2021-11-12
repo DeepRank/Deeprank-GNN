36b2574e5e779338850f1e57bdf7d613ebdd9f8b
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-11-12 17:57:39 +0100

    refact test_nn

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -6,7 +6,30 @@ from deeprank_gnn.foutnet import FoutNet
 from deeprank_gnn.sGAT import sGAT
 
 
-__PLOT__ = False
+def _model_base_test(database, model, plot):
+
+    NN = NeuralNet(database, model,
+                   node_feature=['type', 'polarity', 'bsa',
+                                 'depth', 'hse', 'ic', 'pssm'],
+                   edge_feature=['dist'],
+                   target='irmsd',
+                   index=None,
+                   task='reg',
+                   batch_size=64,
+                   percent=[0.8, 0.2])
+
+    NN.train(nepoch=5, validate=True)
+
+    NN.save_model('test.pth.tar')
+
+    NN_cpy = NeuralNet(database, model,
+                       pretrained_model='test.pth.tar')
+
+    if plot:
+        NN.plot_scatter()
+        NN.plot_loss()
+        NN.plot_acc()
+        NN.plot_hit_rate()
 
 
 class TestNeuralNet(unittest.TestCase):
@@ -15,67 +38,13 @@ class TestNeuralNet(unittest.TestCase):
         self.database = 'tests/hdf5/1ATN_residue.hdf5'
 
     def test_ginet(self):
-        NN = NeuralNet(self.database, GINet,
-                       node_feature=['type', 'polarity', 'bsa',
-                                     'depth', 'hse', 'ic', 'pssm'],
-                       edge_feature=['dist'],
-                       target='irmsd',
-                       index=None,
-                       task='reg',
-                       batch_size=64,
-                       percent=[0.8, 0.2])
-
-        NN.train(nepoch=5, validate=False)
-
-        NN.save_model('test.pth.tar')
-
-        NN_cpy = NeuralNet(self.database, GINet,
-                           pretrained_model='test.pth.tar')
-
-        if __PLOT__:
-            NN.plot_scatter()
+        _model_base_test(self.database, GINet, True)
 
     def test_fout(self):
-        NN = NeuralNet(self.database, FoutNet,
-                       node_feature=['type', 'polarity', 'bsa',
-                                     'depth', 'hse', 'ic', 'pssm'],
-                       edge_feature=['dist'],
-                       target='irmsd',
-                       index=None,
-                       task='reg',
-                       batch_size=64,
-                       percent=[0.8, 0.2])
-
-        NN.train(nepoch=5, validate=False)
-
-        NN.save_model('test.pth.tar')
-
-        NN_cpy = NeuralNet(self.database, FoutNet,
-                           pretrained_model='test.pth.tar')
-
-        if __PLOT__:
-            NN.plot_scatter()
+        _model_base_test(self.database, FoutNet, False)
 
     def test_sgat(self):
-        NN = NeuralNet(self.database, sGAT,
-                       node_feature=['type', 'polarity', 'bsa',
-                                     'depth', 'hse', 'ic', 'pssm'],
-                       edge_feature=['dist'],
-                       target='irmsd',
-                       index=None,
-                       task='reg',
-                       batch_size=64,
-                       percent=[0.8, 0.2])
-
-        NN.train(nepoch=5, validate=False)
-
-        NN.save_model('test.pth.tar')
-
-        NN_cpy = NeuralNet(self.database, sGAT,
-                           pretrained_model='test.pth.tar')
-
-        if __PLOT__:
-            NN.plot_scatter()
+        _model_base_test(self.database, sGAT, False)
 
 
 if __name__ == "__main__":
