b11b364262db53d56d339d8e148b3205d79a387b
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-11-15 13:44:23 +0100

    clean up

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -10,6 +10,15 @@ class TestGraph(unittest.TestCase):
     def setUp(self):
         self.graph = Graph()
         self.graph.h52nx('tests/hdf5/1ATN_residue.hdf5', '1ATN_1w')
+        self.ref = 'tests/data/pdb/1ATN/1ATN_2w.pdb'
+
+    def test_score(self):
+        self.graph.get_score(self.ref)
+
+    def test_nx2h5(self):
+        f5 = h5py.File('test_graph.hdf5', 'w')
+        self.graph.pdb = 'tests/data/pdb/1ATN/1ATN_1w.pdb'
+        self.graph.nx2h5(f5)
 
     def test_plot_2d(self):
         self.graph.plotly_2d('1ATN', disable_plot=True)
