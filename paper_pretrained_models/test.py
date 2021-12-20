5b0f429e91c038bc960bf3c78d80ca4ac9e69940
Manon Reau <manon.reau@gmail.com>
2021-12-20 15:11:20 +0100

    Update test.py

--- paper_pretrained_models/test.py
+++ paper_pretrained_models/test.py
@@ -8,22 +8,22 @@ from deeprank_gnn.GraphGenMP import GraphHDF5
 from deeprank_gnn.NeuralNet import NeuralNet
 from deeprank_gnn.ginet import GINet
 
-### path to the docking models in pdb format
+### Graph generation section
+#path to the docking models in pdb format
 pdb_path = '../tests/data/pdb/1ATN/' 
-### path to the pssm files
+#path to the pssm files
 pssm_path = '../tests/data/pssm/1ATN/'
 
 GraphHDF5(pdb_path=pdb_path, pssm_path=pssm_path,
         graph_type='residue', outfile='1ATN_residue.hdf5', nproc=4)
 
-### applu the pre-trained model
+### Prediction section
 pretrained_model = 'fold6_treg_yfnat_b128_e20_lr0.001_4.pt'
 gnn = GINet
 
-### path to the graph(s)
+#path to the graph(s)
 database_test = glob.glob('./*.hdf5')
 
-### Make the prediction
 start_time = time.time()
 model = NeuralNet(database_test, gnn, pretrained_model = pretrained_model)    
 model.test(threshold=None)
